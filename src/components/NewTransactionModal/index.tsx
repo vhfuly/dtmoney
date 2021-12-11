f2558e22fae6dd23419a3823a674cfe10014d1f9
Victor Hugo Silva Fuly <victor_hugo_fuly@hotmail.com>
2021-12-11 16:23:40 -0300

    feat: isert transaction on api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets//income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { api } from '../../services/api';
 
 Modal.setAppElement('#root');
 
@@ -20,9 +21,13 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-        console.log({
-            title,value,category,type
-        })
+        const data = {
+            title,
+            value,
+            category,
+            type,
+        }
+        api.post('/transactions', data)
     }
 
     return (
